6f99fd46b6031f83a4a4b8d8008527a4dd761110
Gaurav <gauravwagh0506@gmail.com>
2024-05-26 20:32:23 +0530

    refresh

--- src/App.jsx
+++ src/App.jsx
@@ -2,35 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import axios from 'axios'
 
 function App() {
   const [count, setCount] = useState(0)
-  const [data, setdata] = useState({});
-
-  const fetchdata = async() => {
-    try {
-      // console.log(import.meta.env.VITE_URL);
-
-      // const ans = await axios.get(import.meta.env.VITE_URL + "/users");
-      // console.log(ans);
-      // console.log("here 1")
-      // const response = await axios.get("/users");
-      // console.log("here 2")
-      // console.log(response)
-      console.log("here 3")
-      
-      const res = await axios.get("/api/users");
-      console.log("here 4")
-      console.log(res)
-
-      // setdata(response)
-
-    } catch (error) {
-      console.log(error)
-    }
-  }
-  fetchdata();
 
   return (
     <>
@@ -47,7 +21,6 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <h1>env variable is here :- {import.meta.env.VITE_MY_VARIABLE}</h1>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
--- vite.config.js
+++ vite.config.js
@@ -1,67 +1,7 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// export default ({ mode }) => {
-//   // Load environment variables based on the mode
-//   const env = loadEnv(mode, process.cwd(), '');
-
-//   return defineConfig({
-//     server: {
-//       proxy: {
-//         '/api': {
-//           target: env.VITE_URL,
-//           changeOrigin: true,
-//           rewrite: (path) => path.replace(/^\/api/, '')
-//         },
-//         '/users':"https://jsonplaceholder.typicode.com/users"
-//       }
-//     },
-//     plugins: [react()]
-//   })
-// };
-
-// export default defineConfig(({ command, mode }) => {
-//   // Load env file based on `mode` in the current working directory.
-//   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-//   const env = loadEnv(mode, process.cwd(), '');
-
-//   return {
-//     // define: {
-//     //   __VITE_URL__: env.VITE_URL,
-//     // },
-//     server:{
-//       proxy:{
-//         '/users':{
-//           target:env.VITE_URL,
-//           changeOrigin:true
-//         },
-//         '/api':{
-//           target:env.VITE_URL,
-//           changeOrigin:true,
-//           rewrite:  (path) => path.replace(/^\/api/, ''),
-//         }
-//       }
-//     },
-//         plugins: [react()]
-//   }
-// })
-
-export default ({ mode }) => {
-  // Load app-level env vars to node-level env vars.
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-
-  return defineConfig({
-    server: {
-      proxy: {
-        '/api': {
-          target: process.env.VITE_API_BASE_URL,
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, '')
-        },
-        '/users': "https://jsonplaceholder.typicode.com/users"
-      }
-    },
-    plugins: [react()],
-    base: "/demo-repo/"
-  });
-}
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
